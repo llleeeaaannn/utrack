835d87bc8be8a815ef6196859563306ef5245d91
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 01:14:00 +0000

    Seperated countries and genders into own variables

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -1,9 +1,12 @@
 export default Users
 
+const Regions = ['United States', 'APAC', 'Latin America', 'Europe']
+const Genders = ['Male', 'Female']
+
 const Users = Array.from({ length: 1000}, (id) => ({
   id: id + 1,
   birthday: Math.ceil(Math.random() * 12),
   spend: Math.floor(Math.random() * 5001),
-  region: ['United States', 'APAC', 'Latin America', 'Europe'][Math.floor(Math.random() * 4)],
-  gender: ['Male', 'Female'][Math.floor(Math.random() * 2)]
+  region: Regions[Math.floor(Math.random() * 4)],
+  gender: Genders[Math.floor(Math.random() * 2)]
 }))
