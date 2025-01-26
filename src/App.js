7a1e4aa094bee972e5fa7583ab21ca38e7d99a6e
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 02:19:39 +0000

    Fixed slider import after testing and commented out unfiltered table for ease of testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Users from "./dataGenerator.js"
 import UserTable from "./userTable.js"
-import SpendSlider from "../sliders/spendSlider.js"
+import SpendSlider from "./sliders/spendSlider.js"
 import React, { useState, useEffect } from 'react'
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div>
-      <UserTable users={users} />
+      // <UserTable users={users} />
 
 
 
