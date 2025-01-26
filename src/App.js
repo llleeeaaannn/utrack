b29e6aa2d248a6accb2df07f1f0a2db4ed20c992
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 02:33:45 +0000

    Import gender dropdown, created gender state and useefect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Users from "./dataGenerator.js"
 import UserTable from "./userTable.js"
 import SpendSlider from "./sliders/spendSlider.js"
+import GenderDropdown from "./sliders/genderDropdown.js"
 import React, { useState, useEffect } from 'react'
 
 function App() {
 
   const [minSpend, setMinSpend] = useState(0);
+  const [gender, setGender] = useState('Male');
   const [users, setUsers] = useState(Users);
   const [filteredUsers, setFilteredUsers] = useState(Users);
 
   useEffect(() => {
     setFilteredUsers(Users.filter(user => user.spend >= minSpend));
   }, [minSpend]);
+
+  useEffect(() => {
+    setFilteredUsers(Users.filter(user => user.gender === gender));
+  }, [gender]);
 
   return (
     <div>
