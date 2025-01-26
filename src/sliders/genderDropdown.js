b684b427ffeabbfe8c70e15b7d936de332913479
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 02:38:55 +0000

    Create option for all genders

--- src/sliders/genderDropdown.js
+++ src/sliders/genderDropdown.js
@@ -3,6 +3,7 @@ const GenderDropdown = ({ gender, setGender }) => {
     <div>
       <label>Gender is set to</label>
       <select value={gender} onChange={(e) => setGender(e.target.value)}>
+        <option value="All">All</option>
         <option value="Male">Male</option>
         <option value="Female">Female</option>
       </select>
