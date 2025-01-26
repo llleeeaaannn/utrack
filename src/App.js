a2d2e970edfa0a4a05c6d81ebc98dbb777ba4b95
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 15:27:31 +0000

    Renamed spendslider file to minspendslider and fixed imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Users from "./dataGenerator.js"
 import UserTable from "./userTable.js"
-import MinSpendSlider from "./filters/spendSlider.js"
+import MinSpendSlider from "./filters/minSpendSlider.js"
 import GenderDropdown from "./filters/genderDropdown.js"
 import RegionDropdown from "./filters/regionDropdown.js"
 import React, { useState, useEffect } from 'react'
