bb7f628fc1457fba585480130deeecb8c13f0955
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 15:41:12 +0000

    registering chart comps

--- src/chart.js
+++ src/chart.js
@@ -1,3 +1,5 @@
 import React from "react"
 import { Line } from "react-chartjs-2"
 import { Chart, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js"
+
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement)
