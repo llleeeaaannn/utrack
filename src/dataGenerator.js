4ebd23d6976b9db577c00760fee83e4b19146280
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 01:44:07 +0000

    Fixed array data generation (id generating incorrectly)

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -2,7 +2,7 @@ const Regions = ['United States', 'APAC', 'Latin America', 'Europe']
 const Genders = ['Male', 'Female']
 
 // Generates an array of 1,000 length
-const Users = Array.from({ length: 1000}, (id) => ({
+const Users = Array.from({ length: 1000}, ( unusedElement, id) => ({
   id: id + 1,
   birthday: Math.ceil(Math.random() * 12),
   spend: Math.floor(Math.random() * 5001),
