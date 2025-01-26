c1edb2b1302056e610fc9ceb858a94e09fd583b8
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 01:17:03 +0000

    Added comments and moved to id++

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -3,8 +3,9 @@ export default Users
 const Regions = ['United States', 'APAC', 'Latin America', 'Europe']
 const Genders = ['Male', 'Female']
 
+// Generates an array of 1,000 length
 const Users = Array.from({ length: 1000}, (id) => ({
-  id: id + 1,
+  id: id++,
   birthday: Math.ceil(Math.random() * 12),
   spend: Math.floor(Math.random() * 5001),
   region: Regions[Math.floor(Math.random() * 4)],
