7163ae735c39de76adbb18c687309086aa123b72
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 01:17:38 +0000

    Revered id++ change

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -5,7 +5,7 @@ const Genders = ['Male', 'Female']
 
 // Generates an array of 1,000 length
 const Users = Array.from({ length: 1000}, (id) => ({
-  id: id++,
+  id: id + 1,
   birthday: Math.ceil(Math.random() * 12),
   spend: Math.floor(Math.random() * 5001),
   region: Regions[Math.floor(Math.random() * 4)],
