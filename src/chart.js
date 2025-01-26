505c9019207e069e7d1a508f9e721e60b5995c12
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 15:50:08 +0000

    Commented plan for the chart'

--- src/chart.js
+++ src/chart.js
@@ -3,3 +3,21 @@ import { Line } from "react-chartjs-2"
 import { Chart, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js"
 
 Chart.register(CategoryScale, LinearScale, PointElement, LineElement)
+
+// Aim: The graph should plot the count of users that remain after the filters, bucketed by their month of birth (the "monthly" line). A second line (labelled "cumulative" in the diagram above) should show the accumulated cost over the course of the year if every one of those users was given (and redeemed) a $5 discount. So if the "monthly" line was plotted at 10 and 12 for January and February, for example, the "cumulative" line would be plotted at $50 and $110 (22 × $5).
+
+// So users line shows how many users are left in that month (after filtering)
+// Cumulative spend line shows the cumulative cost of the discounts
+
+// X axis is just the months
+
+// Potentially have 2 y axis (left + right) for clearer reading as cumilative values should be much higher
+const ReponsiveChart = ({ users }) => {
+
+  // Define the charts data
+
+  // Define chart options (title, legends..)
+
+  // Return chart with the LINE component
+
+}
