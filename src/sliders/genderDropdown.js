8e5690dd69e66ea7fd69de1eaf6becc9ce5f388e
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 02:28:53 +0000

    Gender dropdown file and logic

---
+++ src/sliders/genderDropdown.js
@@ -0,0 +1,13 @@
+const genderDropdown = (gender, setGender) => {
+  return (
+    <div>
+      <label>Gender is set to</label>
+      <select value={gender} onChange={(e) => setGender(e.target.value)}>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
+    </div>
+  )
+}
+
+export default genderDropdown
