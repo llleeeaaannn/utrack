d23d69845f3451954f1c0b2cd1573ad10cc29baf
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 01:18:56 +0000

    Added array.length instead of hard coding lengths

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -8,6 +8,6 @@ const Users = Array.from({ length: 1000}, (id) => ({
   id: id + 1,
   birthday: Math.ceil(Math.random() * 12),
   spend: Math.floor(Math.random() * 5001),
-  region: Regions[Math.floor(Math.random() * 4)],
-  gender: Genders[Math.floor(Math.random() * 2)]
+  region: Regions[Math.floor(Math.random() * Regions.length)],
+  gender: Genders[Math.floor(Math.random() * Genders.length)]
 }))
