19b09512b34f4199f5848ce4e8595abdbeb24e77
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 01:26:44 +0000

    Created user table file with basic format

---
+++ src/userTable.js
@@ -0,0 +1,23 @@
+
+
+
+const UserTable = ({ users }) => {
+  return (
+    <table>
+      <thead>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+      </thead>
+      <tbody>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th></th>
+      </tbody>
+    </table>
+  )
+}
