9c85d7c0f62310b472ab9852c49ef92d68e4e7fe
llleeeaaannn <piercefrank10@gmail.com>
2025-01-26 01:12:26 +0000

    Added export and changed birthday & spend generation logic

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -1,9 +1,9 @@
-
+export default Users
 
 const Users = Array.from({ length: 1000}, (id) => ({
   id: id + 1,
-  birthday: Math.floor(Math.random() * 12) + 1,
-  spend: Math.floor(Math.random() * 5000),
+  birthday: Math.ceil(Math.random() * 12),
+  spend: Math.floor(Math.random() * 5001),
   region: ['United States', 'APAC', 'Latin America', 'Europe'][Math.floor(Math.random() * 4)],
   gender: ['Male', 'Female'][Math.floor(Math.random() * 2)]
 }))
